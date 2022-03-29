2aa4e55d07019af70fbd76a179894cc4ec6b0e8b
João Lenon <lenonSec7@gmail.com>
2022-03-29 19:07:05 -0300

    feat: add default color mode as dark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,11 @@ const config = {
   themeConfig:
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       {
+        colorMode: {
+          defaultMode: 'dark',
+          disableSwitch: false,
+          respectPrefersColorScheme: false,
+        },
         navbar: {
           title: 'Athenna',
           logo: {
