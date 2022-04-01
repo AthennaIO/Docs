6788f09e2eb91a074decf71dddc242141a7cfaee
João Lenon <lenonSec7@gmail.com>
2022-04-01 16:25:23 -0300

    feat: add meta theme color for safari

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
+import Head from '@docusaurus/Head';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -31,6 +32,10 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Athenna is a Node.js framework that focus in many types of application development, such as Http servers, workers, CLI's and more!">
+      <Head>
+          <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"/>
+          <meta name="theme-color" content="#242526" media="(prefers-color-scheme: dark)"/>
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
