f8a314d3a0722f0b8d139e077c127f813396f027
João Lenon <lenonSec7@gmail.com>
2022-04-12 11:37:00 -0300

    feat: add plugin-pwa to docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
+import Head from '@docusaurus/Head';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -31,6 +32,18 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Athenna is a Node.js framework that focus in many types of application development, such as Http servers, workers, CLI's and more!">
+      <Head>
+          <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"/>
+          <meta name="theme-color" content="#242526" media="(prefers-color-scheme: dark)"/>
+          <link rel="icon" href="img/logo.png"/>
+          <link rel="manifest" href="manifest.json"/>
+          <link rel="apple-touch-icon" href="img/logo.png"/>
+          <link rel="mask-icon" href="img/logo.svg"/>
+          <meta name="apple-mobile-web-app-capable" content="yes"/>
+          <meta name="msapplication-TileImage" content="img/logo.png"/>
+          <meta name="msapplication-TileColor" content="#000"/>
+          <meta name="apple-mobile-web-app-status-bar-style" content="#000"/>
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
