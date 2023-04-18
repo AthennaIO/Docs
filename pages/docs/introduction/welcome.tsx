f224207f2d3949f7ac75b1fc4aabac348fcfea4a
João Lenon <lenonSec7@gmail.com>
2023-04-18 00:32:05 -0300

    fix(pages): remove comments from welcome

--- pages/docs/introduction/welcome.tsx
+++ pages/docs/introduction/welcome.tsx
@@ -34,31 +34,31 @@ export default class Welcome extends Component {
           />
         </Box>
 
-        {/*<Box mt='md'>*/}
-        {/*  <Heading as='h2' size='xl'>Introduction</Heading>*/}
+        <Box mt='md'>
+          <Heading as='h2' size='xl'>Introduction</Heading>
 
-        {/*  <Paragraph align='justify'>*/}
-        {/*    Athenna is the plug & play Node.js framework that focuses on giving a very reliable*/}
-        {/*    foundation for the developer to build many types of different applications, perfect*/}
-        {/*    for working with micro-services architecture. All of this foundation was created based*/}
-        {/*    on Laravel, so expect an enjoyable, beautiful, peaceful and productive experience.*/}
-        {/*  </Paragraph>*/}
-        {/*</Box>*/}
+          <Paragraph align='justify'>
+            Athenna is the plug & play Node.js framework that focuses on giving a very reliable
+            foundation for the developer to build many types of different applications, perfect
+            for working with micro-services architecture. All of this foundation was created based
+            on Laravel, so expect an enjoyable, beautiful, peaceful and productive experience.
+          </Paragraph>
+        </Box>
 
-        {/*<Box mt='md'>*/}
-        {/*  <Heading as='h2' size='xl'>Useful Links</Heading>*/}
-        {/*  <List variant='unordered' color='white' p='none' mb='sm'>*/}
-        {/*    <li><Link href='https://github.com/AthennaIO'>Github</Link></li>*/}
-        {/*    <li><Link href='https://discord.gg/vaZgRUkMsm'>Discord</Link></li>*/}
-        {/*    <li><Link href='https://github.com/AthennaIO/Core/blob/main/LICENSE'>License</Link></li>*/}
-        {/*  </List>*/}
+        <Box mt='md'>
+          <Heading as='h2' size='xl'>Useful Links</Heading>
+          <List variant='unordered' color='white' p='none' mb='sm'>
+            <li><Link href='https://github.com/AthennaIO'>Github</Link></li>
+            <li><Link href='https://discord.gg/vaZgRUkMsm'>Discord</Link></li>
+            <li><Link href='https://github.com/AthennaIO/Core/blob/main/LICENSE'>License</Link></li>
+          </List>
 
-        {/*  <List variant='unordered' color='white' p='none'>*/}
-        {/*    <li><Link href='mailto:lenon@athenna.io'>Email</Link></li>*/}
-        {/*    <li><Link href='https://github.com/AthennaIO/Core/issues'>Issues</Link></li>*/}
-        {/*    <li><Link href='https://github.com/AthennaIO/Core/discussions'>Discussions</Link></li>*/}
-        {/*  </List>*/}
-        {/*</Box>*/}
+          <List variant='unordered' color='white' p='none'>
+            <li><Link href='mailto:lenon@athenna.io'>Email</Link></li>
+            <li><Link href='https://github.com/AthennaIO/Core/issues'>Issues</Link></li>
+            <li><Link href='https://github.com/AthennaIO/Core/discussions'>Discussions</Link></li>
+          </List>
+        </Box>
       </Box>
     )
   }
