c56cca8a302b6de41757e8e6e99687dc723ca6da
João Lenon <lenonSec7@gmail.com>
2023-04-29 17:26:26 -0300

    feat(pages): start writing facades page

--- pages/docs/architecture-concepts/facades.tsx
+++ pages/docs/architecture-concepts/facades.tsx
@@ -1,4 +1,6 @@
 import Docs from '#layouts/Docs'
+import Topic from '#components/Topic'
+import Topics from '#components/Topics'
 
 import { Box } from 'dracula-ui'
 import { Component } from 'react'
@@ -18,9 +20,38 @@ export async function getStaticProps() {
 export default class Facades extends Component {
   public static Layout = Docs
 
+  public topics = [
+    { title: 'Introduction' },
+    { title: 'When to use facades' },
+    { title: 'How facades works' },
+    { title: 'Facades class reference' },
+    { title: 'Writing facades' }
+  ]
+
   public render() {
     return (
       <Box>
+        <Topics topics={this.topics}/>
+
+        <Box mt='lg'>
+          <Topic size='xl' pb='xs'>Introduction</Topic>
+        </Box>
+
+        <Box mt='md'>
+          <Topic size='xl' pb='xs'>When to use facades</Topic>
+        </Box>
+
+        <Box mt='md'>
+          <Topic size='xl' pb='xs'>How facades works</Topic>
+        </Box>
+
+        <Box mt='md'>
+          <Topic size='xl' pb='xs'>Facades class reference</Topic>
+        </Box>
+
+        <Box mt='md'>
+          <Topic size='xl' pb='xs'>Writing facades</Topic>
+        </Box>
       </Box>
     )
   }
